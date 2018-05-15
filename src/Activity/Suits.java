552158fe9b10b61ff0cb9a41982d43dd7a7be03a
Sathish2905 <sathishkumart@logiwareinc.in>
2018-05-15 18:18:28 +0530

    first commit

---
+++ src/Activity/Suits.java
@@ -0,0 +1,17 @@
+package Activity;
+
+/**
+ *
+ * The 4 suits of playing cards
+ */
+/**
+ *
+ * @author Sathishkumar T
+ */
+public class Suits {
+
+    public static final int CLUBS = 0;
+    public static final int DIAMONDS = 1;
+    public static final int HEARTS = 2;
+    public static final int SPADES = 3;
+}
